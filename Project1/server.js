58d91ff8368451bf3685dfc5569ad9704edf7694
Wes-McDorman <wmcdorma@ggc.edu>
2016-01-31 03:41:50 -0500

    Added mongojs library with it's first db.cant see straight anymore, good night.  -Wes

--- Project1/server.js
+++ Project1/server.js
@@ -1,6 +1,9 @@
 console.log("Hello from server");
 //loading express
 var express = require('express');
+var express = require('mongojs');
+var db = mongojs("firstDB, []");
+
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
