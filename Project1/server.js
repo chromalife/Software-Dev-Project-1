c8c84efd90b1ce4c3b3dd2e1cbb8451ff0c8637b
Wes-McDorman <wmcdorma@ggc.edu>
2016-01-31 01:09:18 -0500

    Issues with page not loading locally
    Probably CSS issues

--- Project1/server.js
+++ Project1/server.js
@@ -3,8 +3,6 @@ console.log("Hello from server");
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
