503000be143c76b74455082064047b9037fe1bbe
chromalife <anwar.saleeby@gmail.com>
2016-02-08 00:57:54 -0800

    Changed Server Port Number -Anwar.

--- rev_final/To-Do-List-Final/server/config/config.js
+++ rev_final/To-Do-List-Final/server/config/config.js
@@ -3,7 +3,7 @@ var rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = exports = {
 	rootPath: rootPath,
-	port: 3000,
+	port: 3169,
 	db: {
         url: 'mongodb://five:five1234@ds059155.mongolab.com:59155/sdone-todolist'
     }
