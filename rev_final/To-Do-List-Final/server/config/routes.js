07dd9c5b0d0be60ca4f32bfed4f1b22e137ae809
chromalife <anwar.saleeby@gmail.com>
2016-02-10 16:27:26 -0800

    attempted to add photo uploads using s3, uuid and multiparty. Got the button to show up but need to work out kinks for displaying the image on the page, perhaps maybe in a div. -Anwar

--- rev_final/To-Do-List-Final/server/config/routes.js
+++ rev_final/To-Do-List-Final/server/config/routes.js
@@ -14,5 +14,40 @@ function initRoutes(app) {
 	app.post('/api/item/update', TodoItemCtrl.updateTodoItem);
 	app.post('/api/item/delete', TodoItemCtrl.deleteTodoItem);
 }
+var uuid = require('uuid'); // https://github.com/defunctzombie/node-uuid
+var multiparty = require('multiparty'); // https://github.com/andrewrk/node-multiparty
+var s3 = require('s3'); // https://github.com/andrewrk/node-s3-client
+
+var s3Client = s3.createClient({
+	key: '<your_key>',
+	secret: '<your_secret>',
+	bucket: '<your_bucket>'
+});
+
+module.exports = function(app){
+	app.post('/api/v1/upload/image', function(req,res){
+		var from = new multiparty.Form();
+		var file = files.file[0];
+		var contentType = file.headers['content-type'];
+		var extension = file.path.substring(file.path.lastIndexOf('.'));
+		var destPath = '/' + user.id + '/profile' + '/' + uuid.v4 + extension;
+		
+		var headers = {
+			'x-amz-acl': 'public-read',
+			'Content-Length': file.size,
+			'Content-Type': contentType
+		};
+		
+		var uploader = s3Client.upload(file.path, destPath, headers);
+		
+		uploader.on('error', function(err){
+			alert("Looks like you broke it.")
+		});
+		
+		uploader.on('end', function(url){
+			console.log('file opened:', url);
+		});
+	});
+}
 
 module.exports = exports = initRoutes;
--- rev_final/To-Do-List-Final/server/controllers/TodoItemCtrl.js
+++ rev_final/To-Do-List-Final/server/controllers/TodoItemCtrl.js
@@ -42,6 +42,32 @@ function deleteTodoItem(req, res) {
     TodoItem.find({ _id: req.body._id }).remove().exec();
 }
 
+$scope.onFileSelect = function(image){
+	$scope.uploadInProgress = true;
+	$scope.uploadProgress = 0;
+	
+	if(angular.isArray(image)){
+		image = image[0];
+	}
+	
+	$scope.upload = $upload.upload({
+		url:'/api/v1/upload/image',
+		method: 'POST',
+		data: {
+			type:'profile'
+		},
+		file: image
+	}).progress(function(event){
+		$scope.uploadProgress = Math.floor(event.loaded / event.total);
+		$scope.$apply();
+	}).success(function(data, status, header, config){
+		AlertService.success('Photo Uploaded!');
+	}).error(function(err){
+		$scope.uploadInProgress = false;
+		AlertService.error('Error uploading file: ' + err.message || err); 
+	});
+};
+
 
 module.exports.getTodoItems   = exports.getTodoItems   = getTodoItems;
 module.exports.saveTodoItem   = exports.saveTodoItem   = saveTodoItem;
